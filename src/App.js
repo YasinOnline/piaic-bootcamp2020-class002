caeb957466ce4248663e1c76c7933ca60c1684ba
Mohammad Yasin <yasin.online@outlook.com>
2020-11-22 00:47:00 +0500

    Update App.js
    Dummy text added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>This line is added through GitHub Actions</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare vitae erat id dictum. Aliquam erat volutpat. Proin turpis est, consequat ut nisi sed, iaculis efficitur ex. Donec erat quam, congue eget enim sit amet, vehicula condimentum tellus. Donec ultrices vitae risus quis rhoncus. Proin mollis id mauris id interdum. Duis aliquet venenatis arcu, id tempus risus auctor vitae. Suspendisse pulvinar metus vel nibh tempus mollis. Nunc eleifend sagittis urna, cursus euismod lectus maximus nec. Duis porttitor augue at dui fermentum auctor. Aliquam blandit lacinia velit, varius volutpat augue. Integer malesuada neque est, maximus dictum risus porta ut. Ut lorem felis, blandit sed diam sit amet, blandit fermentum velit. Proin pulvinar sapien maximus lorem placerat, varius porttitor lacus ultricies. Vestibulum vel augue non leo mattis tincidunt sit amet vitae justo. Pellentesque sit amet odio ut lacus consectetur posuere quis sed massa.</p>
     </div>
   );
 }
