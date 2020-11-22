d75b38163e9b113c4e25357622e465ab74da92a5
Mohammad Yasin <yasin.online@outlook.com>
2020-11-22 14:40:28 +0500

    Image tag added in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
     <div className="App">
       <h1>This line is added through GitHub Actions</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare vitae erat id dictum. Aliquam erat volutpat. Proin turpis est, consequat ut nisi sed, iaculis efficitur ex. Donec erat quam, congue eget enim sit amet, vehicula condimentum tellus. Donec ultrices vitae risus quis rhoncus. Proin mollis id mauris id interdum. Duis aliquet venenatis arcu, id tempus risus auctor vitae. Suspendisse pulvinar metus vel nibh tempus mollis. Nunc eleifend sagittis urna, cursus euismod lectus maximus nec. Duis porttitor augue at dui fermentum auctor. Aliquam blandit lacinia velit, varius volutpat augue. Integer malesuada neque est, maximus dictum risus porta ut. Ut lorem felis, blandit sed diam sit amet, blandit fermentum velit. Proin pulvinar sapien maximus lorem placerat, varius porttitor lacus ultricies. Vestibulum vel augue non leo mattis tincidunt sit amet vitae justo. Pellentesque sit amet odio ut lacus consectetur posuere quis sed massa.</p>
+      <hr/>
       <div><img src="http://lorempixel.com/400/200" alt=""></img></div>
+      <hr/>
+      <div>copy right (c) <a href="https://www.piaic.org/">PIAIC Pakistan</a></div>
     </div>
   );
 }
